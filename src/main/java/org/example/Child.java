ec82b4ea03f93bd56bf6ccf37145e5758b4fe6e8
HAMSASSIA <sara.hamsassia@capgemini.com>
2022-12-19 17:11:43 +0100

    first commit

---
+++ src/main/java/org/example/Child.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Child {
+
+
+    public int alfa ( ){
+
+        Parent  p =new Parent();
+        int a = p.getUniqueId();
+        return a;
+    }
+
+
+
+
+}
---
+++ src/main/java/org/example/Parent.java
@@ -0,0 +1,11 @@
+package org.example;
+public class Parent {
+    public boolean isAvailable() {
+        return false;
+    }
+    public int getUniqueId() {
+        return 42;
+    }
+
+
+}
